e320e6d9ac4096fa418fdaf7eed6a2a891b5827d
jazqc <cussejazmin@gmail.com>
2022-10-24 18:49:43 -0300

    actualizacion

--- script2.js
+++ script2.js
@@ -10,119 +10,123 @@ let nombre = ""
 let apellido = ""
 let inscripto = false
 
-let curso = Number(prompt("seleccione curso 1 o curso 2"))
+let curso = Number(prompt("seleccione curso 1-Técnicas de extracción de café o curso 2-Recetas de café para principiantes"))
 switch (curso) {
-    case 1: 
-        reserva = prompt("seleccione entre 15/11/2022 o 10/01/2023")
-        cupos = prompt("cantidad de cupos")
-
+    case 1:
+        reserva = prompt("seleccione entre a-15/11/2022 o b-10/01/2023").toLowerCase()
         switch (reserva) {
 
-            case "15/11/2022":
-                while (cuposCurso1a > 0 && inscripto==false)
+            case "a":
+                cupos = prompt("Cantidad de cupos: ")
+                while (cuposCurso1a > 0 && inscripto == false)
                     if (cupos <= cuposCurso1a && cupos > 0) {
                         totalCarrito = totalCarrito + (cupos * 2500)
                         cuposCurso1a = cuposCurso1a - cupos
-                        inscripcion(cupos,cuposCurso1a,totalCarrito)               
+                        inscripcion(cupos, cuposCurso1a, totalCarrito)
                     }
                     else if (cupos > cuposCurso1a && cupos > 0) {
-                        respuesta = prompt("no disponemos de esa cantidad de cupos, quiere reservar menos lugares?")
+                        respuesta = prompt("no disponemos de esa cantidad de cupos, quiere reservar menos lugares? si/no")
                         if (respuesta.toLowerCase() == "si") {
-                            cupos = prompt("cantidad de cupos")
+                            cupos = prompt("cantidad de cupos: ")
                         }
                         else {
-                            break
+                            break;
                         }
                     }
                     else {
-                        break
+                        break;
                     }
-        break;
+                break;
 
-            case "10/01/2023":
-                while (cuposCurso1b > 0)
+            case "b":
+                cupos = prompt("Cantidad de cupos: ")
+                while (cuposCurso1b > 0 && inscripto == false)
                     if (cupos <= cuposCurso1b && cupos > 0) {
                         totalCarrito = totalCarrito + (cupos * 2500)
                         cuposCurso1b = cuposCurso1b - cupos
-                        inscripcion(cupos,cuposCurso1b,totalCarrito)
+                        inscripcion(cupos, cuposCurso1b, totalCarrito)
                         break
                     }
                     else if (cupos > cuposCurso1b && cupos > 0) {
-                        respuesta = prompt("no disponemos de esa cantidad de cupos, quiere reservar menos lugares?")
+                        respuesta = prompt("no disponemos de esa cantidad de cupos, quiere reservar menos lugares?si/no")
                         if (respuesta.toLowerCase() == "si") {
-                            cupos = prompt("cantidad de cupos")
+                            cupos = prompt("cantidad de cupos: ")
                         }
                         else {
-                            break
+                            break;
                         }
                     }
                     else {
-                        break
+                        break;
                     }
-                }  
-            break          
+            
+        }
+        break;
     case 2:
-        reserva = prompt("seleccione entre 10/11/2022 o 03/01/2023")
-        cupos = prompt("cantidad de cupos")
+        reserva = prompt("seleccione entre a-10/11/2022 o b-03/01/2023").toLowerCase()
 
         switch (reserva) {
 
-            case "10/11/2022":
-                while (cuposCurso2a > 0)
+            case "a":
+                cupos = prompt("Cantidad de cupos: ")
+                while (cuposCurso2a > 0 && inscripto == false)
                     if (cupos <= cuposCurso2a && cupos > 0) {
                         totalCarrito = totalCarrito + (cupos * 2000)
                         cuposCurso2a = cuposCurso2a - cupos
-                        inscripcion(cupos,cuposCurso2a,totalCarrito)
+                        inscripcion(cupos, cuposCurso2a, totalCarrito)
                         break
                     }
                     else if (cupos > cuposCurso2a && cupos > 0) {
-                        respuesta = prompt("no disponemos de esa cantidad de cupos, quiere reservar menos lugares?")
+                        respuesta = prompt("no disponemos de esa cantidad de cupos, quiere reservar menos lugares?si/no")
                         if (respuesta.toLowerCase() == "si") {
-                            cupos = prompt("cantidad de cupos")
+                            cupos = prompt("cantidad de cupos: ")
                         }
                         else {
-                            break
+                            break;
                         }
                     }
                     else {
-                        break
+                        break;
                     }
-            break    
+                break;
 
-            case "03/01/2023":
-                while (cuposCurso2b > 0)
+            case "b":
+                cupos = prompt("Cantidad de cupos: ")
+                while (cuposCurso2b > 0 && inscripto == false)
                     if (cupos <= cuposCurso2b && cupos > 0) {
                         totalCarrito = totalCarrito + (cupos * 2000)
                         cuposCurso2b = cuposCurso2b - cupos
-                        inscripcion(cupos,cuposCurso2b,totalCarrito)
+                        inscripcion(cupos, cuposCurso2b, totalCarrito)
                         break
                     }
                     else if (cupos > cuposCurso2b && cupos > 0) {
-                        respuesta = prompt("no disponemos de esa cantidad de cupos, quiere reservar menos lugares?")
+                        respuesta = prompt("no disponemos de esa cantidad de cupos, quiere reservar menos lugares?si/no")
                         if (respuesta.toLowerCase() == "si") {
-                            cupos = prompt("cantidad de cupos")
+                            cupos = prompt("cantidad de cupos: ")
                         }
                         else {
-                            break
+                            break;
                         }
                     }
                     else {
-                        break
+                        break;
                     }
-                    break
+                break;
         }
-        break
-
+        break;
+    default: alert("Ese curso no existe, le recomendamos que visite nuestra tienda de productos")
+        break;
 }
 
 
 
 
-function inscripcion(cupos,cuposCurso,totalPagar) {
-nombre = prompt("Ingrese su nombre")
-apellido = prompt("Ingrese su apellido")
-alert("Inscripción realizada con exito: " + apellido + "," + nombre + " cupos reservados: " + cupos +  " El total a pagar es: " + totalPagar)
-console.log("Cantidad de cupos disponibles: " + cuposCurso + " Nombre y apellido de inscripto: "+ apellido, nombre + " cupos reservados: " + cupos +  " El total a pagar es: $" + totalPagar)
-inscripto = true
-return inscripto
+function inscripcion(cupos, cuposCurso, totalPagar) {
+    nombre = prompt("Ingrese su nombre")
+    apellido = prompt("Ingrese su apellido")
+    alert("Inscripción realizada con exito: " + apellido + "," + nombre + " cupos reservados: " + cupos + " El total a pagar es: " + totalPagar)
+    alert("Le recomendamos que visite nuestra tienda de productos")
+    console.log("Cantidad de cupos disponibles: " + cuposCurso + " Nombre y apellido de inscripto: " + apellido, nombre + " cupos reservados: " + cupos + " El total a pagar es: $" + totalPagar)
+    inscripto = true
+    return inscripto
 }
